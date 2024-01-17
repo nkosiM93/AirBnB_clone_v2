3ce27fd29fc35bd5fc9afd83b26e8659244cad78
AlphaCoder <mathiyathokozanin@gmail.com>
2024-01-17 03:45:17 -0800

    Complete dd_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,7 +19,6 @@ class DBStorage:
 
     __engine = None
     __session = None
-    metadata = None
 
 
     def __init__(self):
@@ -30,23 +29,21 @@ class DBStorage:
                                       f"{os.get_env(HBNB_MYSQL_DB)}, "
                                       "pool_pre_ping=True")
         if os.get_env('HBNB_ENV') == 'test':
-            MetaData(drop_all(self.__engine))
+            Base.metadata.drop_all(self.__engine)
         
     def all(self, cls=None):
         """dictionary of models present in storage"""
         objects = dict()
-        Session = sessionmaker(bind=self.__engine)
-        __session = Session()
         all_classes = (User, State, City, Amenity, Place, Review)
         if cls is None:
             for class_type in all_classes:
-                query = __session.query(class_type)
+                query = self.__session.query(class_type)
                 for obj in query.all():
                     obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
                     objects[obj_key] = obj
 
         else:
-            query = __session.query(cls)
+            query = self.__session.query(cls)
             for obj in query.all():
                 obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
                 objects[obj_key] = obj
@@ -54,10 +51,61 @@ class DBStorage:
 
     def new(self, obj):
         """Adding New objects to DB"""
-        
+
+        if obj is not None:
+            try:
+                self.__session.add(obj)
+                self.__session.flush()
+                self.__session.refresh()
+            except Exception as ex:
+                self.__session.rollback()
+                raise ex
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self):
+        """Removes an object from the storage database"""
+        if obj is not None:
+            self.__session.query(type(obj)).filter(
+                type(obj).id == obj.id).delete(
+                synchronize_session='fetch')
+            self.__session.commit()
+
+    def reload(self):
+        """Load all info from the specific table in database"""
+
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self._session = scoped_session(Session)()
 
 
 
 
 
-    s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
